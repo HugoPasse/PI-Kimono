886ba42427c86acfac16b41d578f569c8bba04aa
avincigu <arthur.vinciguerra@ens-lyon.fr>
2023-11-13 20:13:07 +0100

    commit

--- src/players/sgd_player.py
+++ src/players/sgd_player.py
@@ -11,7 +11,7 @@ class SGDPlayer(OneTurnPlayer):
     def __init__(self, alpha=-1, beta=-1, with_display: bool = False, initial_mdp=PickominoMDP()):
         chosen_alpha, chosen_beta = alpha, beta
         if alpha == -1 or beta == -1:
-            chosen_alpha, chosen_beta = read_params("../../"+parameters.SGD_OUTPUT_FILE)
+            _, chosen_alpha, chosen_beta = read_params("../../"+parameters.SGD_OUTPUT_FILE)
         super().__init__(chosen_alpha, chosen_beta, with_display, initial_mdp)
 
     def play_round(self, available_tiles: List[int], adversary_tiles: List[int], player_last_tile: int) -> (int, int):
